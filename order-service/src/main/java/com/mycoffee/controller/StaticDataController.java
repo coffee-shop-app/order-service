e6d1d5c1b953ddca6d276ed7f3b071c936732bb3
abhijitk7 <abhikulkarni.1988@gmail.com>
2019-08-13 22:48:51 +0530

    Integrated front end code

--- order-service/src/main/java/com/mycoffee/controller/StaticDataController.java
+++ order-service/src/main/java/com/mycoffee/controller/StaticDataController.java
@@ -3,6 +3,7 @@ package com.mycoffee.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ import com.mycoffee.dao.CoffeeTypeRepository;
 import com.mycoffee.entity.CoffeeName;
 import com.mycoffee.entity.CoffeeType;
 
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/data/coffee")
 public class StaticDataController {
